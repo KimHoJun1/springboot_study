0556bcc8256bb00098f08f55c7db4af67efafa06
hjKim <skskskwmf91@gmail.com>
2019-09-18 18:32:27 +0900

    version 1.0

---
+++ src/main/java/me/hjkim/Application.java
@@ -0,0 +1,19 @@
+package me.hjkim;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.WebApplicationType;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Application {
+
+    public static void main(String[] args) {
+        SpringApplication app = new SpringApplication(Application.class);
+        app.setWebApplicationType(WebApplicationType.NONE);
+        app.run(args);
+
+        //new SpringApplicationBuilder().sources(Application.class).run(args);
+
+    }
+}
+
---
+++ src/main/java/me/hjkim/HjkimProperties.java
@@ -0,0 +1,60 @@
+package me.hjkim;
+
+import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.boot.convert.DurationUnit;
+import org.springframework.stereotype.Component;
+import org.springframework.validation.annotation.Validated;
+
+import javax.validation.constraints.NotEmpty;
+import javax.validation.constraints.Size;
+import java.time.Duration;
+import java.time.temporal.ChronoUnit;
+
+@Component
+@Validated
+@ConfigurationProperties("hjkim")
+public class HjkimProperties {
+
+    @NotEmpty
+    private String name;
+
+    @Size( )
+    private int age;
+
+    private String fullName;
+
+    @DurationUnit(ChronoUnit.SECONDS)
+    private Duration sessionTimeout = Duration.ofSeconds(30);
+
+    public Duration getSessionTimeout() {
+        return sessionTimeout;
+    }
+
+    public void setSessionTimeout(Duration sessionTimeout) {
+        this.sessionTimeout = sessionTimeout;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+
+    public String getFullName() {
+        return fullName;
+    }
+
+    public void setFullName(String fullName) {
+        this.fullName = fullName;
+    }
+}
